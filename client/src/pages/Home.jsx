f042b577f70fb01d3ad12dfb9ebdbfb5ff2707fb
abinivesh1696 <abiniveshcsea@gmail.com>
2026-07-09 23:06:14 +0530

    Initial code

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container mt-5">
+        <h2>Home Page</h2>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ client/src/pages/login.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+
+function Login() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container mt-5">
+        <h2>Login Page</h2>
+      </div>
+    </>
+  );
+}
+
+export default Login;
